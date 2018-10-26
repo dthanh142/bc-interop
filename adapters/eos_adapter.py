13b4e8e188a168394e19d23fda3b36c0d690e65d
Timo Hegnauer <t.hegnauer@gmail.com>
2018-10-26 11:26:11 +0200

    get infos about current blocks

--- adapters/eos_adapter.py
+++ adapters/eos_adapter.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pprint
 
 JUNGLE_URL = "http://jungle.cryptolions.io:18888"
 LOCAL_EOS_URL = "http://localhost:8888"
@@ -7,21 +8,47 @@ LOCAL_EOS_URL = "http://localhost:8888"
 
 def get_tx_bin():
 	data = {
-            "code": "eosio.token",
-            "action": "transfer",
-            "args": {
-                "from": "eosio",
-                "to": "noprom",
-                "quantity": "1.0000 EOS",
-                "memo": "created by noprom"
-            }
-    }
+		"code": "eosio.token",
+		"action": "transfer",
+		"args": {
+				"from": "eosio",
+				"to": "noprom",
+			"quantity": "1.0000 EOS",
+			"memo": "test successful"
+		}
+	}
 	r = requests.post(
 		f'{JUNGLE_URL}/v1/chain/abi_json_to_bin', json=data)
 	response = json.loads(r.text)
 	return response["binargs"]
-	
-print(get_tx_bin())
+
+
+def get_head_block_num():
+	r = requests.get(f'{JUNGLE_URL}/v1/chain/get_info')
+	response = json.loads(r.text)
+	return response["head_block_num"]
+
+
+def get_block_num_and_expiration(block_nr):
+	data = {"block_num_or_id": block_nr}
+	r = requests.post(f'{JUNGLE_URL}/v1/chain/get_block', json=data)
+	response = json.loads(r.text)
+	return response["ref_block_prefix"], response["timestamp"]
+
+
+def get_tx_dict():
+	tx = {}
+	tx["data"] = get_tx_bin()
+	tx["ref_block_num"] = get_head_block_num()
+	tx["ref_block_prefix"], tx["expiration"] = get_block_num_and_expiration(tx["ref_block_num"])
+	#increase timestamp by two minutes
+	tx["expiration"] = tx["expiration"][:14] + str(int(tx["expiration"][14:16])+2) + tx["expiration"][16:]
+	return tx
+
+
+pprint.pprint(get_tx_dict())
+
+
 
 def sign_transaction():
 	data = [{
@@ -33,8 +60,8 @@ def sign_transaction():
 			"account": "eosio.token",
 			"name": "transfer",
 			"authorization": [{
-					"actor": "testertimohe",
-					"permission": "active"
+				"actor": "testertimohe",
+				"permission": "active"
 			}],
 			"data": "0000000000ea305500000000487a2b9d102700000000000004454f53000000001163726561746564206279206e6f70726f6d"
 		}],
@@ -46,6 +73,3 @@ def sign_transaction():
 		f'http://localhost:8888/v1/wallet/sign_transaction', json=data)
 	response = json.loads(r.text)
 	return response
-
-
-
