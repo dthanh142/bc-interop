c6ab53a3a69c3501bb7230ee50b2d3696cb8f127
Timo Hegnauer <t.hegnauer@gmail.com>
2018-10-29 16:11:58 +0100

    return block_num and id

--- adapters/eos_adapter.py
+++ adapters/eos_adapter.py
@@ -11,11 +11,6 @@ eos = Eos({
     'key_provider': '5KazRYnXDCNougrvuVtZFDMAiB3kr7M2tjGYNJtQQ2Wn3JFRdTM',
     # get this using 'http://dev.cryptolions.io:38888/v1/chain/get_info'
     # 'chainId': '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
-    'expireInSeconds': 60,
-    'broadcast': True,
-    'debug': True,
-    'sign': True,
-    'verbose': True,
 })
 
 # change needed in pushcontracttrasaction.js
@@ -26,25 +21,25 @@ eos = Eos({
 # })
 
 def make_transaction():
-    return eos.push_transaction('eosio.token', 'transfer', 'jungletimohe', 'active', {
+    response = eos.push_transaction('eosio.token', 'transfer', 'jungletimohe', 'active', {
         "from": "jungletimohe",
         "to": "lioninjungle",
         "quantity": "1.0000 EOS",
         "memo": "testtimo"
     })
+    return response["transaction_id"], response["processed"]["block_num"]
 
 def get_transaction(transaction_id):
     data = {
-        "id": "26cef29a0f19b58084be865e400ce6dbd779296a393a91dff323b17f73700b33"
+        "id": "ee2a44e0b389c1e3c4bc50851c13fbcc7f75fc5a4c06e7b72f6601a25deabe10",
+        "block_num_hint": 21819029
     }
-
     r = requests.post(
-        f'http://jungle.eosmeso.io:8888/v1/history/get_transaction', json=data)
+        f'http://193.93.219.219:8888/v1/history/get_transaction', json=data)
     response = json.loads(r.text)
     return response
 
 
 print(make_transaction())
 # print(get_transaction("tx_id"))
-# print(get_transaction("f7278edea06b657455fc41dafb5df044558cece31816996a47af9416dc11e648"))
 
